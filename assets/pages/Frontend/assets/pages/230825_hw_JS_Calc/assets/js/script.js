db6aa99be7f5d9eb5214a9f9bb8f74fc8d539a3c
gabilgg07 <gabilgg07@gmail.com>
2023-08-29 22:25:30 +0400

    Modified JS Calc Task Codes

--- assets/pages/Frontend/assets/pages/230825_hw_JS_Calc/assets/js/script.js
+++ assets/pages/Frontend/assets/pages/230825_hw_JS_Calc/assets/js/script.js
@@ -17,153 +17,140 @@ showSalaryCalc.addEventListener("click", function () {
       switch (calcCoice) {
         case 1:
           let calcsSalary =
-            "1. Maaşın günə görə hesablanması \n2. Maaşın saata görə hesablanması";
-          let calcSalaryCoice = prompt(
-            "Müvafiq nömrəni daxil etməklə maaşın hesablanma növünü seçin:\n" +
-              calcsSalary
+            "1. Günə görə maaşın hesablanması \n2. Saata görə maaşın hesablanması";
+
+          let calcSalaryCoice;
+
+          do {
+            calcSalaryCoice = prompt(
+              "Maaşın hesablanma növünü seçin:\n" + calcsSalary
+            );
+
+            if (calcSalaryCoice !== null) {
+              calcSalaryCoice = parseInt(calcSalaryCoice);
+              let salary;
+              let months;
+              let days;
+              let hours;
+              let totalSalary;
+              switch (calcSalaryCoice) {
+                case 1:
+                  salary = getMonthSalary();
+                  months = getCountDate("ayların");
+                  days = getCountDate("günlərin");
+
+                  totalSalary = months * salary + days * (salary / 26);
+
+                  alert(`Alacağınız maaş miqdarı: ${totalSalary.toFixed(2)}`);
+
+                  return;
+                case 2:
+                  salary = getMonthSalary();
+                  months = getCountDate("ayların");
+                  days = getCountDate("günlərin");
+                  hours = getCountDate("saatların");
+
+                  totalSalary =
+                    months * salary + (days * 8 + hours) * (salary / 208);
+
+                  alert(`Alacağınız maaş miqdarı: ${totalSalary.toFixed(2)}`);
+
+                  return;
+                default:
+                  alert("Yanlış seçim (─ ‿ ─)");
+              }
+            } else {
+              alert("Seçimdən imtina etdiniz, əsas menuya qayıdırıq (*′☉.̫☉)");
+            }
+          } while (
+            calcSalaryCoice !== null &&
+            (calcSalaryCoice !== 1 || calcSalaryCoice !== 2)
           );
 
-          let salary;
-          do {
-            salary = parseInt(prompt("Aylıq maaşınızı daxil edin:"));
-            if (isNaN(salary)) {
-              alert("Maaş düzgün daxil edilməyib");
-            }
-          } while (isNaN(salary));
-
-          if (calcSalaryCoice !== null) {
-            calcSalaryCoice = parseInt(calcSalaryCoice);
-            let months;
-            let days;
-            let hours;
-            let totalSalary;
-            switch (calcSalaryCoice) {
-              case 1:
-                do {
-                  months = parseInt(
-                    prompt(
-                      "İşlədiyiniz ayların sayını daxil edin (yoxdursa 0 daxil edin):"
-                    )
-                  );
-                  if (isNaN(months)) {
-                    alert("Say düzgün daxil edilməyib");
-                  }
-                } while (isNaN(months));
-
-                do {
-                  days = parseInt(
-                    prompt(
-                      "İşlədiyiniz günlərin sayını daxil edin (yoxdursa 0 daxil edin):"
-                    )
-                  );
-                  if (isNaN(days)) {
-                    alert("Say düzgün daxil edilməyib");
-                  }
-                } while (isNaN(days));
-
-                totalSalary = months * salary + days * (salary / 26);
-
-                alert(`Alacağınız maaş miqdarı: ${totalSalary.toFixed(2)}`);
-
-                return;
-              case 2:
-                do {
-                  months = parseInt(
-                    prompt(
-                      "İşlədiyiniz ayların sayını daxil edin (yoxdursa 0 daxil edin):"
-                    )
-                  );
-                  if (isNaN(months)) {
-                    alert("Say düzgün daxil edilməyib");
-                  }
-                } while (isNaN(months));
-
-                do {
-                  days = parseInt(
-                    prompt(
-                      "İşlədiyiniz günlərin sayını daxil edin (yoxdursa 0 daxil edin):"
-                    )
-                  );
-                  if (isNaN(days)) {
-                    alert("Say düzgün daxil edilməyib");
-                  }
-                } while (isNaN(days));
-
-                do {
-                  hours = parseInt(
-                    prompt(
-                      "İşlədiyiniz saatların sayını daxil edin (yoxdursa 0 daxil edin):"
-                    )
-                  );
-                  if (isNaN(hours)) {
-                    alert("Say düzgün daxil edilməyib");
-                  }
-                } while (isNaN(hours));
-
-                totalSalary =
-                  months * salary + (days * 8 + hours) * (salary / 208);
-
-                alert(`Alacağınız maaş miqdarı: ${totalSalary.toFixed(2)}`);
-
-                return;
-              default:
-                alert("Yanlış seçim");
-            }
-          } else {
-            alert("Canceled");
-          }
           break;
         case 2:
           let age = prompt("Yaşınızı daxil edin: ");
           if (age !== null) {
             age = parseInt(age);
             if (age >= 18 && age <= 65) {
-              let weight;
-              do {
-                weight = prompt("Çəkinizi daxil edin (kq-la): ");
-                if (weight !== null) {
-                  weight = parseInt(weight);
-                  if (weight <= 0 || isNaN(weight)) {
-                    alert("Çəkinizi düzgün daxil edin");
-                  }
-                } else {
-                  alert("İmtina etdiniz");
-                }
-              } while ((weight <= 0 || isNaN(weight)) && weight !== null);
-              let height;
-              do {
-                height = prompt("Boyunuzu daxil edin (sm-lə): ");
-                if (height !== null) {
-                  height = parseInt(height);
-                  if (height <= 0 || isNaN(height)) {
-                    alert("Boyunuzu düzgün daxil edin");
-                  }
-                } else {
-                  alert("İmtina etdiniz");
-                }
-              } while ((height <= 0 || isNaN(height)) && height !== null);
+              let weight = getNumFromPrompt(
+                "Çəkinizi daxil edin (kq-la): ",
+                "Çəkinizi düzgün daxil edin",
+                "İmtina etdiniz"
+              );
 
-              if (height > 0) {
-                let bmi = weight / (height / 100) ** 2;
-                alert(`Bədən Kütlə İndeksiniz: ${bmi.toFixed(1)}`);
-                return;
-              } else {
-              }
+              if (!weight) return;
+              let height = getNumFromPrompt(
+                "Boyunuzu daxil edin (sm-lə): ",
+                "Boyunuzu düzgün daxil edin",
+                "İmtina etdiniz"
+              );
+              if (!height) return;
+
+              let bmi = weight / (height / 100) ** 2;
+              alert(`Bədən Kütlə İndeksiniz: ${bmi.toFixed(1)}`);
+              return;
             } else {
-              alert("BMI düsturu sizin yaşınıza uyğun deyil");
+              alert("BMI düsturu sizin yaşınıza uyğun deyil (｡•́‿ •̀｡)");
+              return;
             }
           } else {
-            alert("İmtina etdiniz");
+            alert("Seçimdən imtina etdiniz, əsas menuya qayıdırıq (︶ω︶)");
           }
           break;
         case 3:
-          alert("Sistemdən çıxdınız");
-          break;
+          alert("Sistemdən çıxdınız (╥﹏╥)");
+          return;
         default:
-          alert("Yanlış seçim");
+          alert("Yanlış seçim (｡- .•)");
       }
     } else {
-      alert("İmtina etdiniz");
+      alert("Proqramdan istifadə etdiyiniz üçün təşəkkürlər (๑✧◡✧๑)");
+      return;
     }
-  } while (calcCoice !== 3 && calcCoice !== null);
+  } while (calcCoice !== 3);
 });
+
+function getCountDate(text) {
+  let countDate;
+  do {
+    countDate = parseInt(
+      prompt(`İşlədiyiniz ${text} sayını daxil edin (yoxdursa 0 daxil edin):`)
+    );
+    if (isNaN(countDate)) {
+      alert("Say düzgün daxil edilməyib ( · ❛ ֊ ❛)");
+    }
+  } while (isNaN(countDate));
+
+  return countDate;
+}
+
+function getMonthSalary() {
+  let salary;
+  do {
+    salary = parseInt(prompt("Aylıq maaşınızı daxil edin:"));
+    if (isNaN(salary)) {
+      alert("Maaş düzgün daxil edilməyib");
+    }
+  } while (isNaN(salary));
+
+  return salary;
+}
+
+function getNumFromPrompt(infoTxt, warningTxt, cancelTxt) {
+  let num;
+  do {
+    num = prompt(infoTxt);
+    if (num !== null) {
+      num = parseInt(num);
+      if (num <= 0 || isNaN(num)) {
+        alert(warningTxt);
+      }
+    } else {
+      alert(cancelTxt);
+      return;
+    }
+  } while ((num <= 0 || isNaN(num)) && num !== null);
+
+  return num;
+}
